18cd6f7c5b7f10846241a68f4d0cc2004d7e96de
Poletti, Davide <dpoletti@fr.imshealth.com>
2020-11-24 15:55:10 +0100

    Added code in the main and CodeShare

--- src/main/java/org/dpoletti/interview/hrhospital/HrHospital.java
+++ src/main/java/org/dpoletti/interview/hrhospital/HrHospital.java
@@ -4,6 +4,29 @@ public class HrHospital {
 
 	
 	public static final void main(String args[]) {
+		Department d = new Department();
+		Doctor doc = new Doctor();
+		doc.name="Doc1";
+		doc.basicSalary=20;
+		doc.workingHours=160; 
+		Nurse n = new Nurse();
+		n.name="Nurse1";
+		n.nightSalary=10;
+		n.basicSalary=5; 
+		n.workingHours=100;
+		n.nigthHour=20; 
 		
+		d.addWorker(doc);
+		d.addWorker(n);
+		
+		ChirurgicalDoctor cdoc = new ChirurgicalDoctor();
+		cdoc.name="Doc1";
+		cdoc.basicSalary=20;
+		cdoc.workingHours=160; 
+		cdoc.operations=10;
+		cdoc.bonusOperation=100; 
+		
+		d.addWorker(cdoc);
+		System.out.println(d.calculateCost());
 	}
 }
