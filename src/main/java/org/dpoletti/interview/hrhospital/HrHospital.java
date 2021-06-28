29f7ef4f4f674da88bf1d16b2c05025618d51556
Poletti, Davide <dpoletti@fr.imshealth.com>
2021-06-28 13:03:41 +0200

    improved names

--- src/main/java/org/dpoletti/interview/hrhospital/HrHospital.java
+++ src/main/java/org/dpoletti/interview/hrhospital/HrHospital.java
@@ -4,29 +4,29 @@ public class HrHospital {
 
 	
 	public static final void main(String args[]) {
-		Department d = new Department();
-		Doctor doc = new Doctor();
-		doc.name="Doc1";
-		doc.basicSalary=20;
-		doc.workingHours=160; 
-		Nurse n = new Nurse();
-		n.name="Nurse1";
-		n.nightSalary=10;
-		n.basicSalary=5; 
-		n.workingHours=100;
-		n.nigthHour=20; 
+		Department department = new Department();
+		Doctor doctor1 = new Doctor();
+		doctor1.name="Doc1";
+		doctor1.basicSalary=20;
+		doctor1.workingHours=160; 
+		Nurse nurse1 = new Nurse();
+		nurse1.name="Nurse1";
+		nurse1.nightSalary=10;
+		nurse1.basicSalary=5; 
+		nurse1.workingHours=100;
+		nurse1.nigthHour=20; 
 		
-		d.addWorker(doc);
-		d.addWorker(n);
+		department.addWorker(doctor1);
+		department.addWorker(nurse1);
 		
-		ChirurgicalDoctor cdoc = new ChirurgicalDoctor();
-		cdoc.name="Doc1";
-		cdoc.basicSalary=20;
-		cdoc.workingHours=160; 
-		cdoc.operations=10;
-		cdoc.bonusOperation=100; 
+		ChirurgicalDoctor cdoctor = new ChirurgicalDoctor();
+		cdoctor.name="Doc1";
+		cdoctor.basicSalary=20;
+		cdoctor.workingHours=160; 
+		cdoctor.operations=10;
+		cdoctor.bonusOperation=100; 
 		
-		d.addWorker(cdoc);
-		System.out.println(d.calculateCost());
+		department.addWorker(cdoctor);
+		System.out.println(department.calculateCost());
 	}
 }
